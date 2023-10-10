0f1bb6616b04f303674781e6102833872443d85e
Thomas Crha <thomas@9bitbyte.com>
2023-10-10 20:07:54 +1100

    added nwg-bar for nicer logout/shutdown menu; addded nwg-displays as a display manager; added some beter shortcuts to wezterm; cleaned up and rejiggered sway config;

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -21,31 +21,56 @@ wezterm.on('user-var-changed', function(window, pane, name, value)
   end
   window:set_config_overrides(overrides)
 end)
-return {
-  color_scheme = 'OneHalfDark',
-  font_size = 9,
-  hide_tab_bar_if_only_one_tab = true,
-  enable_tab_bar = true,
-  enable_scroll_bar = true,
-  window_close_confirmation = "NeverPrompt",
-  audible_bell = 'Disabled',
-  font = wezterm.font('JetBrains Mono'),
-  window_padding = {
-    left = 2,
-    right = 2,
-    top = 0,
-    bottom = 0,
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentTab{confirm=true}
   },
-  -- Make sure word selection stops on most punctuations.
-  --
-  -- Note that dot (.) & slash (/) are allowed though for
-  -- easy selection of (partial) paths.
-  selection_word_boundary = " \t\n{}[]()\"'`,;:@│*",
-
-  --  Defualt Program
-  --
-
-  -- default_prog = { 'wsl.exe', '-d', 'Ubuntu-22.04', 'tmux', 'new', '-s', 'neovim', '||', 'tmux', 'a', '-t', 'neovim' }
-  -- default_prog = { '/bin/zsh', '-c', 'tmux', 'new', '-s', 'scratch', '||', 'tmux', 'a', '-t', 'scratch' }
+  {
+    key = 't',
+    mods = 'CTRL',
+    action = wezterm.action{SpawnTab='CurrentPaneDomain'}
+  },
+  {
+    key = 'v',
+    mods = 'CTRL',
+    action = wezterm.action{PasteFrom="Clipboard"}
+  },
 }
+config.color_scheme = 'OneHalfDark'
+config.font_size = 9
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = true
+config.enable_scroll_bar = true
+config.window_close_confirmation = "NeverPrompt"
+config.audible_bell = 'Disabled'
+config.font = wezterm.font('JetBrains Mono')
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+-- Make sure word selection stops on most punctuations.
+--
+-- Note that dot (.) & slash (/) are allowed though for
+-- easy selection of (partial) paths.
+config.selection_word_boundary = " \t\n{}[]()\"'`,;:@│*"
+--  Defualt Program
+--
 
+-- config.default_prog = { 'wsl.exe', '-d', 'Ubuntu-22.04', 'tmux', 'new', '-s', 'neovim', '||', 'tmux', 'a', '-t', 'neovim' }
+-- config.default_prog = { '/bin/zsh', '-c', 'tmux', 'new', '-s', 'scratch', '||', 'tmux', 'a', '-t', 'scratch' }
+
+return config
