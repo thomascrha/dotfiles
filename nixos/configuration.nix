8aecef95744b8d6318f6e575c837213cc88a2a1b
Thomas Crha <thomas@9bitbyte.com>
2024-07-01 17:23:38 +1000

    added correct nixos folder to ignore stow list

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,7 @@
   networking.hostName = "nixos"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+
 # Configure network proxy if necessary
 # networking.proxy.default = "http://user:password@proxy:port/";
 # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -29,6 +30,7 @@
   time.timeZone = "Australia/Sydney";
 
 # Select internationalisation properties.
+
   i18n.defaultLocale = "en_GB.UTF-8";
 
   i18n.extraLocaleSettings = {
