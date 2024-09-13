ab28be6c8f452961f762037c2374f1d3b5401131
Thomas Crha <thomas@9bitbyte.com>
2024-09-13 22:51:54 +1000

    Here are 10 commit suggestions based on the provided diff:

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,17 +1,17 @@
 return {
   {
     "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
+    -- event = "VeryLazy",
+    -- lazy = false,
+    -- version = false, -- set this if you want to always pull the latest change
     opts = {
       provider = "copilot"
     },
-    config = function()
-
-      require("avante_lib").load()
-
-      end,
+    -- config = function()
+    --
+    --   require("avante_lib").load()
+    --
+    --   end,
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
