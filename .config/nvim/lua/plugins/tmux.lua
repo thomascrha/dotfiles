d2a30a155cfda938020755dbae03d0250bcb17ad
Thomas Crha <thomas@9bitbyte.com>
2024-09-16 18:55:24 +1000

    feat(tmux): navigate panes with Alt+hjkl feat(navigation): standardize tmux and neovim pane navigation

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -8,18 +8,18 @@ return {
         disable_when_zoomed = true -- defaults to false
       }
 
-      vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-      vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-      vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-      vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+      vim.keymap.set('n', "<A-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set('n', "<A-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set('n', "<A-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set('n', "<A-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
 
-      vim.keymap.set('n', "<C-Left>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-      vim.keymap.set('n', "<C-Down>", nvim_tmux_nav.NvimTmuxNavigateDown)
-      vim.keymap.set('n', "<C-Up>", nvim_tmux_nav.NvimTmuxNavigateUp)
-      vim.keymap.set('n', "<C-Right>", nvim_tmux_nav.NvimTmuxNavigateRight)
+      vim.keymap.set('n', "<A-Left>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set('n', "<A-Down>", nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set('n', "<A-Up>", nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set('n', "<A-Right>", nvim_tmux_nav.NvimTmuxNavigateRight)
 
-      vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-      vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+      vim.keymap.set('n', "<A-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+      vim.keymap.set('n', "<A-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
 
     end
   }
