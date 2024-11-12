da909b6f4c3d3a5aac5371c51ae6d923597761b2
Thomas Crha <thomas@9bitbyte.com>
2024-11-12 19:10:51 +1100

    feat(config/rofi): Implement custom Rofi theme based on deadguy's sidetab chore(config/sway): Relocate Xwayland title formatting rule refactor(config/sway): Consolidate window rules for floating and focus chore(config/systemd): Enable KDE Connect indicator service fix(scripts/lock-routine): Use swaylock instead of swaylock-effects for screen locking

--- .config/sway/scripts/lock-routine.sh
+++ .config/sway/scripts/lock-routine.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-running_apps=$(pgrep -fc "swaylock-effects")
-if [ "$running_apps" = 0 ]; then
+running_apps=$(pgrep -fc "swaylock")
+if [ $running_apps = 0 ]; then
     LT="$lock_timeout" ST="$screen_timeout" LT=${LT:-300} ST=${ST:-300}
     swayidle -w \
-        timeout $LT 'swaylock-effects -f' \
+        timeout $LT 'swaylock -f' \
         timeout $((LT + ST)) 'swaymsg "output * power off"' \
                       resume 'swaymsg "output * power on"'  \
-        timeout $ST 'pgrep -xu "$USER" -f swaylock-effects >/dev/null && swaymsg "output * power off"' \
-             resume 'pgrep -xu "$USER" -f swaylock-effects >/dev/null && swaymsg "output * power on"'  \
-        before-sleep 'swaylock-effects -f' \
-        lock 'swaylock-effects -f' \
-        unlock 'pkill -xu "$USER" -SIGUSR1 -f swaylock-effects'
+        timeout $ST 'pgrep -xu "$USER" -f swaylock >/dev/null && swaymsg "output * power off"' \
+             resume 'pgrep -xu "$USER" -f swaylock >/dev/null && swaymsg "output * power on"'  \
+        before-sleep 'swaylock -f' \
+        lock 'swaylock -f' \
+        unlock 'pkill -xu "$USER" -SIGUSR1 -f swaylock'
 fi
 
