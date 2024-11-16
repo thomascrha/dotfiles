5c8d647550e41024cb3f338767bfecdd49d17d07
Thomas Crha <thomas@9bitbyte.com>
2024-11-16 12:29:57 +1100

    feat(sway/autostart): Implement systemd-based autostart management refactor(sway): Centralize autostart configuration in systemd refactor(sway/config): Migrate autostart to systemd user services

---
+++ .config/systemd/user/check-restart-services.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get all service files in the current directory
+for service in *.service; do
+    # get the service name
+    service_name=$(basename $service .service)
+    # check if the service is running
+    service_status=$(systemctl --user is-active $service_name)
+    echo "Service $service_name is $service_status"
+    # mathc against active or activating
+    if [[ $service_status != "active" || $service_status != "activating" ]]; then
+        # restart the service
+        systemctl --user restart $service_name &
+    fi
+    # systemctl --user restart $service_name &
+done
