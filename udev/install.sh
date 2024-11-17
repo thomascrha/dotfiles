dc48703843d1888e7961eb843deec4282de753d6
Thomas Crha <thomas@9bitbyte.com>
2024-11-17 16:43:26 +1100

    fix(udev): Remove sudo check and use sudo in install script

--- udev/install.sh
+++ udev/install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
-# check if sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 set -xe
 
-install -m 755 set-power-profile /usr/local/bin
-install -m 644 etc_udev_rules.d/60-onbattery.rules /etc/udev/rules.d
-install -m 644 etc_udev_rules.d/61-onacpower.rules /etc/udev/rules.d
-install -m 644 etc_udev_rules.d/62-lowbattery.rules /etc/udev/rules.d
+sudo install -m 755 set-power-profile /usr/local/bin
+sudo install -m 644 etc_udev_rules.d/60-onbattery.rules /etc/udev/rules.d
+sudo install -m 644 etc_udev_rules.d/61-onacpower.rules /etc/udev/rules.d
+sudo install -m 644 etc_udev_rules.d/62-lowbattery.rules /etc/udev/rules.d
 
