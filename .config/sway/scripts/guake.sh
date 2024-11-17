0c145985c1dab1ef9d330d3a23d9cdbd513fe517
Thomas Crha <thomas@9bitbyte.com>
2024-11-17 19:05:48 +1100

    refactor(sway/config): Replace direct terminal invocation with Guake script fix(sway/terminal): Correct dropdown terminal sizing and positioning by caching window geometry

--- .config/sway/scripts/guake.sh
+++ .config/sway/scripts/guake.sh
@@ -1,9 +1,54 @@
 #!/bin/bash
 
-# check is wezterm is running
-if [[ ! $(ps aux | grep terminal_scratchpad | grep -v grep) ]]; then
-    echo "Starting wezterm"
-    WEZTERM_GUAKE=on wezterm start --class terminal_scratchpad
+DEFAULT_HEIGHT=70
+MAX_HEIGHT=${GUAKE_MAX_HEIGHT:-970}
+MAX_WIDTH=${GUAKE_MAX_WIDTH:-1800}
+# get the current size of the dropdown terminal window
+# WIDTH=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "dropdown-terminal") | .window_rect | "\(.width)"')
+HEIGHT=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "dropdown-terminal") | .window_rect | "\(.height)"')
+
+# CURRENT_WIDTH_PPT=$((100 * $WIDTH / $MAX_WIDTH))
+CURRENT_HEIGHT_PPT=$((100 * $HEIGHT / $MAX_HEIGHT))
+
+# fetch the old values of the dropdown terminal window - if don't exist use the values given by the window as
+# sway aplys some default values to the window
+
+if [[ -f /tmp/guake_size ]]; then
+    # WIDTH_PPT=$(cat /tmp/guake_size | cut -d' ' -f1)
+    HEIGHT_PPT=$(cat /tmp/guake_size | cut -d' ' -f2)
+else
+    # WIDTH_PPT=100
+    HEIGHT_PPT=$DEFAULT_HEIGHT
 fi
-# run sway scratchpad toggle
-swaymsg [app_id="terminal_scratchpad"] scratchpad show
+
+if [[ $CURRENT_HEIGHT_PPT -lt 10 ]]; then
+    HEIGHT_PPT=$DEFAULT_HEIGHT
+fi
+
+# write the current values to a file
+echo "$CURRENT_WIDTH_PPT $CURRENT_HEIGHT_PPT" > /tmp/guake_size
+
+/usr/bin/swaymsg '[app_id="dropdown-terminal"] scratchpad show' || \
+    WEZTERM_GUAKE=on /usr/bin/wezterm start --class dropdown-terminal
+
+/usr/bin/swaymsg "[app_id=\"dropdown-terminal\"] resize set 100ppt ${HEIGHT_PPT}ppt, move position 0 0"
+
+
+
+#
+# show existing or start new dropdown terminal
+# bindsym Ctrl+Grave exec swaymsg '[app_id="$ddterm-id"] scratchpad show' \
+  # || $ddterm && swaymsg '[app_id="$ddterm-id"] $ddterm-resize'
+# swaymsg '[app_id="dropdown-terminal"]'
+# set $ddterm-id dropdown-terminal
+# set $ddterm WEZTERM_GUAKE=on $terminal start --class $ddterm-id
+# set $ddterm-resize resize set 100ppt 100ppt, move position 0 0
+#
+# # resize/move new dropdown terminal windows
+# for_window [app_id="$ddterm-id"] {
+#   floating enable
+#   $ddterm-resize
+#   move to scratchpad
+#   scratchpad show
+# }
+
