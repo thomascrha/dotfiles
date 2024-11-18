21efb4f22cc40bd6bec1f9c445443fc0027e7d7a
Thomas Crha <thomas@9bitbyte.com>
2024-11-18 15:10:36 +1100

    fix(scripts/guake): Calculate max height dynamically based on focused output fix(scripts/guake): Account for gaps and borders in max height calculation fix(scripts/guake): Correctly persist and restore Guake height percentage

--- .config/sway/scripts/guake.sh
+++ .config/sway/scripts/guake.sh
@@ -4,33 +4,34 @@
 ## with the sway IPC.
 ## Requires: sway, swaymsg, jq, wezterm, bc
 
+
 DEFAULT_HEIGHT=70
-MAX_HEIGHT=${GUAKE_MAX_HEIGHT:-1802}
+GAPS_AND_BORDERS=50
+MAX_HEIGHT=$(echo "($(swaymsg -t get_outputs | jq '.[] | select(.focused == true) | .rect | .height') - $GAPS_AND_BORDERS)" | bc)
+
+echo "MAX_HEIGHT: $MAX_HEIGHT"
 
 HEIGHT=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "dropdown-terminal") | .window_rect | "\(.height)"')
 
-# CURRENT_WIDTH_PPT=$((100 * $WIDTH / $MAX_WIDTH))
 CURRENT_HEIGHT_PPT=$(echo "($HEIGHT * 100 + $MAX_HEIGHT - 1) / $MAX_HEIGHT" | bc)
-echo $CURRENT_HEIGHT_PPT
 
 if [[ -f /tmp/guake_size ]]; then
     # WIDTH_PPT=$(cat /tmp/guake_size | cut -d' ' -f1)
-    HEIGHT_PPT=$(cat /tmp/guake_size | cut -d' ' -f2)
+    HEIGHT_PPT=$(cat /tmp/guake_size)
 else
     # WIDTH_PPT=100
     HEIGHT_PPT=$DEFAULT_HEIGHT
 fi
 
-if [[ $CURRENT_HEIGHT_PPT -lt 20 ]]; then
+if [[ $HEIGHT_PPT -lt 20 ]]; then
     HEIGHT_PPT=$DEFAULT_HEIGHT
 fi
 
-if [[ $CURRENT_HEIGHT_PPT -gt 100 ]]; then
+if [[ $HEIGHT_PPT -gt 100 ]]; then
     HEIGHT_PPT=100
 fi
 
-# write the current values to a file
-echo "$CURRENT_WIDTH_PPT $CURRENT_HEIGHT_PPT" > /tmp/guake_size
+echo $CURRENT_HEIGHT_PPT > /tmp/guake_size
 
 /usr/bin/swaymsg '[app_id="dropdown-terminal"] scratchpad show' || \
     WEZTERM_GUAKE=on /usr/bin/wezterm start --class dropdown-terminal
