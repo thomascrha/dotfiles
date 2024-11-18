0732735411f431e9dcf89e093eef2c1cf263a68f
Thomas Crha <thomas@9bitbyte.com>
2024-11-18 15:37:46 +1100

    fix(scripts/guake): Persist Guake height between sessions feat(scripts/guake): Initialize Guake height to default if not set fix(scripts/guake): Correctly calculate and persist Guake height percentage refactor(scripts/guake): Simplify Guake height management logic

--- .config/sway/scripts/guake.sh
+++ .config/sway/scripts/guake.sh
@@ -4,24 +4,22 @@
 ## with the sway IPC.
 ## Requires: sway, swaymsg, jq, wezterm, bc
 
-
 DEFAULT_HEIGHT=70
+if [[ ! -f /tmp/guake_size ]]; then
+    echo $DEFAULT_HEIGHT > /tmp/guake_size
+fi
+
 GAPS_AND_BORDERS=50
 MAX_HEIGHT=$(echo "($(swaymsg -t get_outputs | jq '.[] | select(.focused == true) | .rect | .height') - $GAPS_AND_BORDERS)" | bc)
 
 echo "MAX_HEIGHT: $MAX_HEIGHT"
 
-HEIGHT=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "dropdown-terminal") | .window_rect | "\(.height)"')
+HEIGHT=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "dropdown-terminal") | .window_rect | "\(.height)"' || $(cat /tmp/guake_size))
 
 CURRENT_HEIGHT_PPT=$(echo "($HEIGHT * 100 + $MAX_HEIGHT - 1) / $MAX_HEIGHT" | bc)
 
-if [[ -f /tmp/guake_size ]]; then
-    # WIDTH_PPT=$(cat /tmp/guake_size | cut -d' ' -f1)
-    HEIGHT_PPT=$(cat /tmp/guake_size)
-else
-    # WIDTH_PPT=100
-    HEIGHT_PPT=$DEFAULT_HEIGHT
-fi
+HEIGHT_PPT=$(cat /tmp/guake_size)
+echo "HEIGHT_PPT: $HEIGHT_PPT"
 
 if [[ $HEIGHT_PPT -lt 20 ]]; then
     HEIGHT_PPT=$DEFAULT_HEIGHT
@@ -31,10 +29,17 @@ if [[ $HEIGHT_PPT -gt 100 ]]; then
     HEIGHT_PPT=100
 fi
 
-echo $CURRENT_HEIGHT_PPT > /tmp/guake_size
+if [[ ! -z $CURRENT_HEIGHT_PPT ]]; then
+    echo $CURRENT_HEIGHT_PPT > /tmp/guake_size
+fi
 
-/usr/bin/swaymsg '[app_id="dropdown-terminal"] scratchpad show' || \
-    WEZTERM_GUAKE=on /usr/bin/wezterm start --class dropdown-terminal
+/usr/bin/swaymsg '[app_id="dropdown-terminal"] scratchpad show'
+
+# wezterm is not running inside the dropdown-terminal scratchpad
+if [[ $(echo $?) -ne 0 ]]; then
+    WEZTERM_GUAKE=on /usr/bin/wezterm start --class dropdown-terminal & 
+    sleep 0.4
+fi
 
 /usr/bin/swaymsg "[app_id=\"dropdown-terminal\"] resize set 100ppt ${HEIGHT_PPT}ppt, move position 0 0"
 
