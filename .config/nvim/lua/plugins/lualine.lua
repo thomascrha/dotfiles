d697fe9108dafedc1cb4d47cb12347bee610b441
Thomas Crha <thomas@9bitbyte.com>
2024-12-13 20:41:29 +1100

    fixed lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,25 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     config = function()
-      require("lualine").setup({ theme = 'onedark' })
+      require("lualine").setup({
+        theme = 'onehalfdark',
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,  -- 1 for relative path, 2 for absolute path
+              file_status = true,
+              newfile_status = true,
+              on_click = function()
+                local filename = vim.fn.expand('%:.')  -- %:. gives relative path from current working directory
+                vim.fn.setreg('"', filename)
+                -- vim.notify('Copied relative path to clipboard: ' .. filename, vim.log.levels.INFO)
+              end,
+            }
+          }
+        }
+      })
+
     end,
   },
 }
