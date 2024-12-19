ba797f48628c39dae66e40dea840fb5de0bc4846
Thomas Crha <thomas@9bitbyte.com>
2024-12-19 21:18:42 +1100

    feat(wezterm/config): implement modal pane resizing refactor(wezterm/config): migrate pane resizing to modal plugin feat(wezterm/config): add support for modal keybindings via plugin

---
+++ .config/wezterm/modes/resize.lua
@@ -0,0 +1,40 @@
+local wezterm = require('wezterm')
+local M = {}
+
+function M.setup(modal)
+
+  local act = wezterm.action
+  -- Define the resize mode key bindings
+  local key_table = {
+    { key = 'LeftArrow', action = act.AdjustPaneSize { 'Left', 10 } },
+    { key = 'LeftArrow', mods = 'SHIFT', action = act.AdjustPaneSize { 'Left', 3 } },
+
+    { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 10 } },
+    { key = 'RightArrow', mods = 'SHIFT', action = act.AdjustPaneSize { 'Right', 3 } },
+
+    { key = 'UpArrow', action = act.AdjustPaneSize { 'Up', 10 } },
+    { key = 'UpArrow', mods = 'SHIFT', action = act.AdjustPaneSize { 'Up', 3 } },
+
+    { key = 'DownArrow', action = act.AdjustPaneSize { 'Down', 10 } },
+    { key = 'DownArrow', mods = 'SHIFT', action = act.AdjustPaneSize { 'Down', 3 } },
+
+    -- Cancel the mode by pressing escape
+    { key = "Escape", action = modal.exit_mode("resize") },
+    { key = "c", mods = "CTRL", action = modal.exit_mode("resize") },
+  }
+
+  -- Define the status text for resize mode
+  local status_text = wezterm.format({
+    { Attribute = { Intensity = "Bold" } },
+    { Foreground = { Color = "Yellow" } },
+    { Text = wezterm.nerdfonts.ple_left_half_circle_thick },
+    { Foreground = { Color = "Black" } },
+    { Background = { Color = "Yellow" } },
+    { Text = "RESIZE  " },
+  })
+
+  -- Register the resize mode
+  modal.add_mode("resize", key_table, status_text)
+end
+
+return M
--- systemd/install.sh
+++ systemd/install.sh
@@ -16,12 +16,12 @@ for file in *.service; do
     echo "Reloading systemd daemon"
     # reload the systemd daemon
     systemctl --user daemon-reload
-    echo "Enabling $service_name"
+    echo "Enabling and starting $service_name"
     # enable the service
-    systemctl --user enable $service_name
-    echo "Starting $service_name"
+    systemctl --user enable --now $service_name
+    # echo "Starting $service_name"
     # start the service
-    systemctl --user start $service_name
+    # systemctl --user start $service_name
 done
 
 echo "Installing timers"
@@ -31,8 +31,8 @@ for file in *.timer; do
     sudo install -m 644 $file /usr/lib/systemd/user/
     echo "Reloading systemd daemon"
     systemctl --user daemon-reload
-    echo "Enabling $timer_name"
-    systemctl --user enable $timer_name
-    echo "Starting $timer_name"
-    systemctl --user start $timer_name
+    echo "Enabling and starting $timer_name"
+    systemctl --user enable --now $timer_name
+    # echo "Starting $timer_name"
+    # systemctl --user start $timer_name
 done
