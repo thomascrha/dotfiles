b1d8db5061302b4c3ec2992422d63310abc3ec0b
Thomas Crha <thomas@9bitbyte.com>
2025-03-24 12:11:03 +1100

    Added thinkfan install and config Added better ai completion within nvim

--- decrypt.sh
+++ decrypt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 AGE_FILES=$(fd . ~/.config/ ~/.local/ | grep -e '\.age$')
+AGE_FILES="$AGE_FILES $HOME/.zshenv.age"
 for FILE in $AGE_FILES; do
     FILENAME=$(echo $FILE | sed 's/.age//')
     if ! [ -f $FILENAME ]; then
