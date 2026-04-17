2e1c88a529bbb9b4a23d1f947df3d1ab16a3db2d
Thomas Crha <thomas@9bitbyte.com>
2026-04-17 13:27:46 +1000

    updated treesitter for nvim12

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,58 +1,78 @@
 -- Highlight, edit, and navigate code
 return {
   {
-    "romus204/tree-sitter-manager.nvim",
-    dependencies = {}, -- tree-sitter CLI must be installed system-wide
-    config = function()
-      require("tree-sitter-manager").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "diff",
-          "html",
-          "lua",
-          "luadoc",
-          "markdown",
-          "markdown_inline",
-          "query",
-          "vim",
-          "vimdoc",
-          "yaml",
-          "json",
-          "javascript",
-          "typescript",
-          "tsx",
-          "css",
-          "scss",
-          "rust",
-          "python",
-          "go",
-          "java",
-          "ruby",
-          "php",
-          "sql",
-          "dockerfile",
-          "git_config",
-          "git_rebase",
-          "gitcommit",
-          "gitignore",
-          "toml",
-          "regex",
-          "comment"
-          -- "jsonc"
-        },
-        -- Optional: custom paths
-        -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
-        -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
-      })
-    end
-  },
-  -- {
-  --   'nvim-treesitter/nvim-treesitter',
-  --   dependencies = { 'nvim-lua/plenary.nvim' },
-  --   lazy = false,
-  --   build = ':TSUpdate',
-  -- }
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'main',
+    build = ":TSUpdate",
+    main = "nvim-treesitter", -- Sets main module to use for opts
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
 
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then
+            return
+          end
+
+          -- check if parser exists and load it
+          if not vim.treesitter.language.add(language) then
+            return
+          end
+
+          -- enables syntax highlighting and other treesitter features
+          vim.treesitter.start(buf, language)
+
+          -- enables treesitter based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+
+      local ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "css",
+        "scss",
+        "rust",
+        "python",
+        "go",
+        "java",
+        "ruby",
+        "php",
+        "sql",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "toml",
+        "regex",
+        "comment"
+        -- "jsonc"
+      }
+
+      local already_installed = require("nvim-treesitter").get_installed()
+      local to_install = vim.iter(ensure_installed)
+          :filter(function(parser)
+            return not vim.tbl_contains(already_installed, parser)
+          end)
+          :totable()
+      require("nvim-treesitter").install(to_install)
+    end,
+  },
 }
 -- vim: set ft=lua ts=2 sts=2 sw=2 et:
